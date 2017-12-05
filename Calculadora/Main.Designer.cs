bcc73e257373f80681153f1af925d6c72ff2a4e9
Welson Lima <welsonlimawlsn@gmail.com>
2017-12-05 17:34:50 -0200

    Identificação de Valores

---
+++ Calculadora/Main.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Calculadora
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxAlgoritimo = new System.Windows.Forms.TextBox();
+            this.buttonCalcular = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.White;
+            this.buttonExit.FlatAppearance.BorderSize = 0;
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.Font = new System.Drawing.Font("Trebuchet MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonExit.Location = new System.Drawing.Point(230, 0);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(54, 30);
+            this.buttonExit.TabIndex = 2;
+            this.buttonExit.Text = "x";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Escreva o algoritimo:";
+            // 
+            // textBoxAlgoritimo
+            // 
+            this.textBoxAlgoritimo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBoxAlgoritimo.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxAlgoritimo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxAlgoritimo.Location = new System.Drawing.Point(12, 52);
+            this.textBoxAlgoritimo.MaxLength = 30;
+            this.textBoxAlgoritimo.Name = "textBoxAlgoritimo";
+            this.textBoxAlgoritimo.Size = new System.Drawing.Size(260, 19);
+            this.textBoxAlgoritimo.TabIndex = 0;
+            // 
+            // buttonCalcular
+            // 
+            this.buttonCalcular.BackColor = System.Drawing.Color.White;
+            this.buttonCalcular.FlatAppearance.BorderSize = 0;
+            this.buttonCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonCalcular.Font = new System.Drawing.Font("Trebuchet MS", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalcular.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.buttonCalcular.Location = new System.Drawing.Point(185, 77);
+            this.buttonCalcular.Name = "buttonCalcular";
+            this.buttonCalcular.Size = new System.Drawing.Size(86, 34);
+            this.buttonCalcular.TabIndex = 1;
+            this.buttonCalcular.Text = "Calcular";
+            this.buttonCalcular.UseVisualStyleBackColor = false;
+            this.buttonCalcular.Click += new System.EventHandler(this.buttonCalcular_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "CALCULADORA";
+            // 
+            // Main
+            // 
+            this.AcceptButton = buttonCalcular;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(152)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(284, 128);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonCalcular);
+            this.Controls.Add(this.textBoxAlgoritimo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonExit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxAlgoritimo;
+        private System.Windows.Forms.Button buttonCalcular;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Calculadora/Main.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void buttonCalcular_Click(object sender, EventArgs e)
+        {
+            int valor1 = 0;
+            int valor2 = 0;
+            char operacao = ' ';
+            string algoritmo = textBoxAlgoritimo.Text;
+            string sinais = "+-/*";
+
+            foreach(char c in sinais)
+            {
+                if(algoritmo.IndexOf(c) != -1)
+                {
+                    valor1 = int.Parse(algoritmo.Substring(0, algoritmo.IndexOf(c)));
+                    valor2 = int.Parse(algoritmo.Substring(algoritmo.IndexOf(c) + 1));
+                    operacao = c;
+                }
+            }
+            Console.WriteLine(valor1);
+            Console.WriteLine(operacao);
+            Console.WriteLine(valor2);
+        }
+    }
+}
